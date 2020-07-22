9f689e4006bec345f6901a68071940d5af4e25d9
Carl Colglazier <carl@carlcolglazier.com>
2020-07-22 14:29:57 -0400

    tabs

--- init.el
+++ init.el
@@ -1,8 +1,8 @@
 ;; Welcome to my Emacs config! A lot of this was added ad-hoc over time
 ;; or copied from places on the web. Feel free to do likewise.
-;;     -- Carl
+;;		 -- Carl
 
-(server-start) 
+;(server-start)
 
 ;; This contains all the initialization files.
 ;; Better organized that way.
@@ -22,13 +22,11 @@
 ;; Set up use-package
 ;; This is only needed once, near the top of the file
 (eval-when-compile
-  ;; Following line is not needed if use-package.el is in ~/.emacs.d
-  ;(add-to-list 'load-path "<path where use-package is installed>")
-  (require 'use-package))
+	;; Following line is not needed if use-package.el is in ~/.emacs.d
+	;(add-to-list 'load-path "<path where use-package is installed>")
+	(require 'use-package))
 
-(defvar *use-lsp* (>= emacs-major-version 26))
-
-(when *use-lsp*
+(when (>= emacs-major-version 26)
 	(use-package lsp-mode
 		:ensure t
 		:commands (lsp lsp-deferred)
@@ -61,17 +59,17 @@
 	)
 
 (if (fboundp 'with-eval-after-load)
-    (defalias 'after-load 'with-eval-after-load)
-  (defmacro after-load (feature &rest body)
-    "After FEATURE is loaded, evaluate BODY."
-    (declare (indent defun))
-    `(eval-after-load ,feature
-       '(progn ,@body))))
+		(defalias 'after-load 'with-eval-after-load)
+	(defmacro after-load (feature &rest body)
+		"After FEATURE is loaded, evaluate BODY."
+		(declare (indent defun))
+		`(eval-after-load ,feature
+			 '(progn ,@body))))
 
 (when *is-a-mac*
-  (setq ispell-program-name "/usr/local/bin/aspell"))
+	(setq ispell-program-name "/usr/local/bin/aspell"))
 (when (not *is-a-mac*)
-  (setq ispell-program-name "/usr/bin/aspell"))
+	(setq ispell-program-name "/usr/bin/aspell"))
 
 (require 'init-themes)
 (require 'init-osx)
@@ -81,15 +79,15 @@
 (provide 'init-flycheck)
 
 (setq backup-directory-alist
-      `((".*" . ,temporary-file-directory)))
+			`((".*" . ,temporary-file-directory)))
 (setq auto-save-file-name-transforms
-      `((".*" ,temporary-file-directory t)))
+			`((".*" ,temporary-file-directory t)))
 
 ;;----------------------------------------------------------------------------
 ;; Variables configured via the interactive 'customize' interface
 ;;----------------------------------------------------------------------------
 (when (file-exists-p custom-file)
-  (load custom-file))
+	(load custom-file))
 
 ;; Autoload files.
 (global-auto-revert-mode t)
@@ -100,49 +98,85 @@
 (set-face-attribute 'default nil :height 140)
 
 (when (member "DejaVu Sans Mono" (font-family-list))
-  (set-default-font "DejaVu Sans Mono"))
+	(set-default-font "DejaVu Sans Mono"))
 
 (maybe-require-package 'ox-hugo)
 (when (maybe-require-package 'magit)
-  (global-set-key (kbd "C-c g") 'magit-status))
+	(global-set-key (kbd "C-c g") 'magit-status))
 
 (when (maybe-require-package 'company)
-;(require 'company)
-  (add-hook 'after-init-hook 'global-company-mode)
-  (require-package 'company-tern)
-  ;(add-to-list 'company-backends 'company-tern)
-  ;(dolist (backend '(company-eclim company-semantic))
-  ;  (delq backend company-backends))
-  ; TODO: dmininish
-  )
+	(add-hook 'after-init-hook 'global-company-mode)
+	(require-package 'company-tern))
  
 
 (when (require-package 'go-mode)
-  (autoload 'go-mode "go-mode" nil t)
-  (add-to-list 'auto-mode-alist '("\\.go\\'" . go-mode))
+	(autoload 'go-mode "go-mode" nil t)
+	(add-to-list 'auto-mode-alist '("\\.go\\'" . go-mode))
 
-  (defun go-mode-setup ()
-    (add-hook 'before-save-hook 'gofmt-before-save)
-    (define-key (current-local-map) "\C-c\C-c" 'compile)
-    )
+	(defun go-mode-setup ()
+		(add-hook 'before-save-hook 'gofmt-before-save)
+		(define-key (current-local-map) "\C-c\C-c" 'compile)
+		)
 
-  (add-hook 'go-mode-hook 'go-mode-setup)
-  )
+	(add-hook 'go-mode-hook 'go-mode-setup)
+	)
 
 (require-package 'tidal)
 (setq tidal-boot-script-path "~/krepovo/tidal/BootTidal.hs")
 
-(setq-default tab-width 2)
-(setq-default indent-tabs-mode t)
-(defun my-tabs ()
-   (setq indent-tabs-mode t)
-   (setq tab-stop-list (number-sequence 2 200 2))
-   (setq tab-width 2)
-   (setq indent-line-function 'insert-tab))
-
-(add-hook 'org-mode-hook 'my-tabs)
-(add-hook 'python-mode-hook 'my-tabs)
 (setq org-src-preserve-indentation t)
+
+;; tabs
+;; https://dougie.io/emacs/indentation/
+; START TABS CONFIG
+;; Create a variable for our preferred tab width
+(setq custom-tab-width 2)
+
+;; Two callable functions for enabling/disabling tabs in Emacs
+(defun disable-tabs () (setq indent-tabs-mode nil))
+(defun enable-tabs ()
+	(local-set-key (kbd "TAB") 'tab-to-tab-stop)
+	(setq indent-tabs-mode t)
+	(setq tab-width custom-tab-width))
+
+;; Hooks to Enable Tabs
+(add-hook 'prog-mode-hook 'enable-tabs)
+;; Hooks to Disable Tabs
+(add-hook 'lisp-mode-hook 'disable-tabs)
+(add-hook 'emacs-lisp-mode-hook 'disable-tabs)
+
+(add-hook 'python-mode-hook
+					(lambda ()
+						(setq indent-tabs-mode t)
+						(setq python-indent 2)
+						(setq python-tab-width 4))
+						(tabify (point-min) (point-max)))
+
+;; Language-Specific Tweaks
+(setq-default python-indent-offset custom-tab-width) ;; Python
+(setq-default js-indent-level custom-tab-width)			 ;; Javascript
+
+;; Making electric-indent behave sanely
+(setq-default electric-indent-inhibit t)
+
+;; Make the backspace properly erase the tab instead of
+;; removing 1 space at a time.
+(setq backward-delete-char-untabify-method 'hungry)
+
+;; (OPTIONAL) Shift width for evil-mode users
+;; For the vim-like motions of ">>" and "<<".
+;(setq-default evil-shift-width custom-tab-width)
+
+;; WARNING: This will change your life
+;; (OPTIONAL) Visualize tabs as a pipe character - "|"
+;; This will also show trailing characters as they are useful to spot.
+(setq whitespace-style '(face tabs tab-mark trailing))
+(custom-set-faces
+ '(whitespace-tab ((t (:foreground "#636363")))))
+(setq whitespace-display-mappings
+	'((tab-mark 9 [124 9] [92 9]))) ; 124 is the ascii ID for '\|'
+(global-whitespace-mode) ; Enable whitespace mode everywhere
+; END TABS CONFIG
 
 ;; weird menu bar bug on KDE
 ; https://www.birkelbach.eu/post/dealing-with-empty-menus-in-gnu-emacs/
